30f56b2c7e330b7cdccbc7bfacc3a331e0780e28
Chaudhari-Amar <achaudhari@unomaha.edu>
2025-01-17 03:15:18 +0530

    spotlight

--- 3js_bookReader.js
+++ 3js_bookReader.js
@@ -4,6 +4,8 @@ import { GLTFLoader } from "./three/GLTFLoader.js";
 import { OrbitControls } from './three/OrbitControls.js';
 import { RGBELoader } from './three/RGBELoader.js';
 
+import {Tween,Easing} from './tween/tween.esm.js'
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const container = document.getElementById("threejscanvas")
@@ -18,17 +20,31 @@ renderer.setSize( container.clientWidth, container.clientHeight );
 
 const controls = new OrbitControls( camera, renderer.domElement );
 
-camera.position.z = 2;
+camera.position.x = -1;
 camera.position.y = 1;
+camera.position.z = 4;
 
-controls.target.set(0, 1, 0);;
+controls.target.set(-1, 1, 1);;
 //controls.update();
 
-const light = new THREE.AmbientLight( 0x404040 ); // soft white light
-scene.add( light );
+//spotlight
 
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-scene.add( directionalLight );
+const spotLight = new THREE.SpotLight(0xffffff,1,5,0.5,1,0,4); // White light, intensity 1
+spotLight.position.set(-5, 4, -3); // Position the spotlight
+
+scene.add(spotLight);
+
+spotLight.castShadow = true;
+
+// Target for the spotlight
+const spotLightTarget = new THREE.Object3D();
+spotLightTarget.position.set(0, 0, 0); // Point the spotlight at the origin
+scene.add(spotLightTarget);
+spotLight.target = spotLightTarget;
+
+// Add a helper to visualize the spotlight
+const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+//scene.add(spotLightHelper);
 
 const light2 = new THREE.HemisphereLight( 0xffffbb, 0x080820, 0);
 scene.add( light2 );     
@@ -73,7 +89,7 @@ previousBtn.addEventListener("click",function(){
 // ---------------------------------------------------------------------
 new RGBELoader()
 .setPath('./assets/')
-.load('brown_photostudio_01_2k.hdr', function (texture) {
+.load('blue_photo_studio_2k.hdr', function (texture) {
 
     
     texture.mapping = THREE.EquirectangularReflectionMapping;
@@ -84,7 +100,9 @@ new RGBELoader()
     //scene1.background = texture;
     scene.background = new THREE.Color( "rgb(1,22,45)" );
     scene.environment = texture;
-    scene.environment.rotation = 180
+    //scene.environment.rotation = 180
+    //scene.environment.intensity = 0.1;
+    console.log(scene.environment)
 
 });
 
@@ -118,16 +136,104 @@ function ( gltf ) {
 
 
 
+//camera
+
+var tween_eye,tween_target;
+
+var cameraTransitionTime = 2000 //milliseconds
+
+function goToCamera(eye,target){
+
+  //eye
+  var from = {
+    x: camera.position.x,
+    y: camera.position.y,
+    z: camera.position.z
+  };
+  
+  var to = {
+    x: eye[0],
+    y: eye[1],
+    z: eye[2],
+  };
+   tween_eye = new Tween(from,false)
+    .to(to, cameraTransitionTime)
+    .easing(Easing.Quadratic.InOut)
+    .onUpdate(function () {
+    camera.position.set(from.x,from.y,from.z);
+    //camera.lookAt(new THREE.Vector3(0, 0, -0.5));
+    //controls.target = new THREE.Vector3(0, 0, -0.5)
+    controls.update()
+    
+  })
+
+    .start();
 
 
 
+    //target
+  var from_target = {
+    x: controls.target.x,
+    y: controls.target.y,
+    z: controls.target.z
+  };
+  
+  var to_target = {
+    x: target[0],
+    y: target[1],
+    z: target[2],
+  };
+   tween_target = new Tween(from_target,false)
+    .to(to_target, cameraTransitionTime)
+    .easing(Easing.Quadratic.InOut)
+    .onUpdate(function () {
+    //camera.lookAt(from_target.x,from_target.y,from_target.z);
+    //camera.lookAt(new THREE.Vector3(0, 0, -0.5));
+    controls.target.set(from_target.x,from_target.y,from_target.z)
+    //controls.update()
+    
+  })
+    .onComplete(function () {
+    //controls.target.copy(scene.position);
+    //controls.update()
 
-function animate() {
+  })
+    .start();
+}
+
+document.addEventListener("keydown", function(e){
+
+    if(e.key == "l"){
+    var lookAt = (new THREE.Vector3( 0, 0, -0.5 )).applyQuaternion( camera.quaternion ).add( camera.position ); //get lookat vector, 0.5 is distance from camera
+    console.log("[" + camera.position.x + "," + camera.position.y + "," + camera.position.z + "],[" + lookAt.x + "," + lookAt.y + "," + lookAt.z,"]")
+  }
+  
+  
+  if(e.key == "c"){
+  
+    console.log(controls.target)
+  }
+
+  if(e.key=="s"){
+
+    goToCamera([-0.7984355963931029,2.1458759298750048,0.030072785196943196],[-0.7984355923920204,1.6459414820404277,0.021976621393235994 ])
+  }
+});
+
+
+
+function animate(time) {
     requestAnimationFrame( animate );
 
     if (mixer) {
         mixer.update(0.01); // Update animation with time delta
     }
+
+    if(tween_eye){
+        
+        tween_eye.update(time)
+        tween_target.update(time)
+     }
 
 
 
